eb0c8ffd4de537248310a22f592b3b87a3397d31
matsu <yasminnevessiqueira@gmail.com>
2026-03-29 18:30:10 -0300

    automatizando a conexão com o bd

--- sweetmanager/src/main/java/br/com/confeitaria/sweet_manager/Application.java
+++ sweetmanager/src/main/java/br/com/confeitaria/sweet_manager/Application.java
@@ -1,13 +1,42 @@
 package br.com.confeitaria.sweet_manager;
 
+import br.com.confeitaria.sweet_manager.domain.entity.Usuario;
+import br.com.confeitaria.sweet_manager.domain.repository.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Bean
+    CommandLineRunner initDatabase(UsuarioRepository repository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (repository.findByEmail("admin@sweet").isEmpty()) {
+                Usuario admin = new Usuario();
+                admin.setNome("Admin");
+                admin.setEmail("admin@sweet");
+                admin.setSenha(passwordEncoder.encode("admin123"));
+                admin.setRole("ADMIN");
+                repository.save(admin);
+                System.out.println("Usuário ADMIN criado!");
+            }
+
+            if (repository.findByEmail("teste@teste.com").isEmpty()) {
+                Usuario user = new Usuario();
+                user.setNome("Teste");
+                user.setEmail("teste@teste.com");
+                user.setSenha(passwordEncoder.encode("123"));
+                user.setRole("USER");
+                repository.save(user);
+                System.out.println("Usuário COMUM criado!");
+            }
+        };
+    }
 }
